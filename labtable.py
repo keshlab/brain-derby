ea352dd94a14f28790135f5dd9aaddd1852b0eaa
neerajt <neerajtandon25@gmail.com>
2016-08-04 16:00:01 -0500

    updated labtable.py
    Return was being called twice, but python will only evaluate the
    first return statement. So, the function from htmlTable wasn't being
    called at all. Now, the output of the function call is nested in the
    first return statement - so now with 7324470 the table renders
    correctly.

--- labtable.py
+++ labtable.py
@@ -9,31 +9,9 @@ class StringGenerator(object):
         return """<html>
           <head></head>
           <body>
-            <table method="get" action="generate" border="2">
-            <tr>
-            	<th> Name</th>
-            	<th> # of Brains Completed </th>
-            </tr>
-            <tr>
-            	<td>Olivia</td>
-            	<td align="center">25</td>
-            </tr>
-            <tr>
-            	<td>Yunxiang</td>
-            	<td align="center">35</td>
-            </tr>
-            <tr>
-            	<td>Sid</td>
-            	<td align="center">15</td>
-            </tr>
-            <tr>
-            	<td>Anu</td>
-            	<td align="center">20</td>
-            </tr>
-            </table>
+						%s
           </body>
-        </html>"""
-        return htmlTable.CountRaters('BSNIP2')
+        </html>""" % htmlTable.CountRaters('BSNIP2')
 
 if __name__ == '__main__':
     cherrypy.quickstart(StringGenerator())
